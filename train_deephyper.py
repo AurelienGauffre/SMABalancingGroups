58bd6693249ab828c8009b5b5c6a8a3779c26461
gauffrea <aurelien.gauffre@gmail.com>
2024-02-12 01:22:01 +0100

    configs

--- train_deephyper.py
+++ train_deephyper.py
@@ -175,11 +175,11 @@ if __name__ == "__main__":
     # problem.add_hyperparameter((1, 60), "T", default_value=40)
 
     # Define your search and execute it
-    for K in [2,4,8,16]:
+    for K in [2,4,8]:
         # ['erm','jtt', 'suby', 'subg', 'rwy', 'rwg', 'dro']
         #['erm', 'jtt', 'suby']
         #['subg', 'rwy', 'rwg', 'dro']
-        for method in  ['suby','erm','jtt', 'suby', 'subg', 'rwy', 'rwg', 'dro']:
+        for method in  ['erm','jtt', 'suby', 'subg', 'rwy', 'rwg', 'dro']:
             args.method = method
             args.SMA.K = K
             args.group = f"K={args.SMA.K}_{args.method}"
