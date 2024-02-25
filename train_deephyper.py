e06b626bb33e3ca79a1907f0f8c1705cf6c0b1af
gauffrea <aurelien.gauffre@gmail.com>
2024-02-25 22:34:59 +0100

    configs

--- train_deephyper.py
+++ train_deephyper.py
@@ -190,7 +190,7 @@ if __name__ == "__main__":
         args.SMA.name = dataset_name
         # Define your search and execute it
 
-        for K in [4]:
+        for K in [5]:
             args.SMA.K = K
             for mu in [.2,.3,.5]:
 
